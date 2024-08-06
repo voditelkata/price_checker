0e023b4b74220beb92e6fcb46b6f00d0e8d4fa24
voditelkata <70779844+voditelkata@users.noreply.github.com>
2024-08-06 14:47:32 +0300

    Update pairs.js

--- src/pairs.js
+++ src/pairs.js
@@ -1,10 +1,17 @@
-export const pairs = {
-    "6СТ-110А3(0)": ['A090 141 09 0 L', '6CT-55VL 0 REACTOR'],
-    "6СТ-77А3(0)": ['A090 141 09 0 R', '6CT-55VL 0'],
-    "6СТ-77А3(1)": ['A045 143 01 0 R', '6CT-55VL 1'],
-    "6СТ-66 А3(0)": ['A062 141 07 0 L', '6CT-60VL 1'],
-    "6СТ-66 А3(1)": ['A062 141 07 0 R', '6CT-74VL 0'],
-    "6СТ-60 А3(0)": ['A070 141 09 0 L', '6CT-74VL 1'],
-    "6СТ-60 А3(1)": ['A070 141 09 0 R', '6CT-90VL 0'],
-    "6СТ-56 А3(0)": ['A100 231 09 0 R P', '640000010'],
-}
+export const pairs = [
+    {
+        priceCode: '6СТ - 110А3(0)',
+        priceCodeFromBat: ['6CT-62 0 +EFB'],
+        priceCodeFromRimbat: ['A062 141 07 0 L']
+    },
+    {
+        priceCode: '6СТ-77А3(0)',
+        priceCodeFromBat: ['6CT-90VL 0'],
+        priceCodeFromRimbat: ['A070 141 09 0 R']
+    },
+    {
+        priceCode: '6СТ-77А3(1)',
+        priceCodeFromBat: ['6CT-60VL 0 LB'],
+        priceCodeFromRimbat: ['A075 311 09 9 L', 'B090 141 09 0 L']
+    }
+]
