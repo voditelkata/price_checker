9195516e38c7a6db61c60abc4305dce7ffe3d51f
voditelkata <70779844+voditelkata@users.noreply.github.com>
2024-08-08 12:09:18 +0300

    Update pairs.js

--- src/pairs.js
+++ src/pairs.js
@@ -1,27 +1,68 @@
 export const pairs = [
     {
-        productCode: '6СТ - 110А3(0)',
-        productCodeFromBat: '6CT-62 0 +EFB',
-        productCodeFromRimbat: 'A062 141 07 0 L',
+        productCode: '190 322 K11 #O',
+        productCodeFromBat: 'AF 190 З-R-К',
+        productCodeFromRimbat: 'TC-00001647',
     },
     {
-        productCode: '6СТ-77А3(0)',
-        productCodeFromBat: '6CT-90VL 0',
-        productCodeFromRimbat: 'A070 141 09 0 R',
+        productCode: '190 322 K11 #O',
+        productCodeFromBat: '690134024',
+        productCodeFromRimbat: '6СТ-190NR',
     },
     {
-        productCode: '6СТ-77А3(0)',
+        productCode: '140 322 K11 #O',
+        productCodeFromBat: 'AF 140 З-R-К-o',
+        productCodeFromRimbat: '6СТ-140NR',
+    },
+    {
+        productCode: '100 322 K11 #O',
+        productCodeFromBat: '600119024',
+        productCodeFromRimbat: '6СТ-100N',
+    },
+    {
+        productCode: '100 322 K11 #O',
+        productCodeFromBat: '600119024',
+        productCodeFromRimbat: 'TC-00001643',
+    },
+    {
+        productCode: '100 322 K11 #O',
+        productCodeFromBat: '600119024',
+        productCodeFromRimbat: 'E090 311 09 9 L',
+    },
+    {
+        productCode: '090 322 K11 #O',
+        productCodeFromBat: '590120024',
+        // productCodeFromRimbat: 'E090 311 09 9 L',
+    },
+    {
+        productCode: '075 322 K11 #O',
+        productCodeFromBat: '577112024',
+        productCodeFromRimbat: 'E075 321 09 9 R',
+    },
+    {
+        productCode: '075 322 K11 #O',
+        productCodeFromBat: '577112024',
+        productCodeFromRimbat: 'TC-00001640',
+    },
+    {
+        productCode: '075 322 K11 #O',
+        productCodeFromBat: '577112024',
+        productCodeFromRimbat: '6СТ-77N',
+    },
+    {
+        productCode: '060 322 K11 #O',
         productCodeFromBat: '560030000',
-        productCodeFromRimbat: 'A070 141 09 0 R',
+        productCodeFromRimbat: 'E060 321 07 9 L',
     },
     {
-        productCode: '6СТ-77А3(1)',
-        productCodeFromBat: '6CT-60VL 0 LB',
-        productCodeFromRimbat: 'A075 311 09 9 L',
+        productCode: '060 322 K11 #O',
+        productCodeFromBat: '560108024',
+        productCodeFromRimbat: 'TC-00001638',
     },
     {
-        productCode: '6СТ-77А3(1)',
-        productCodeFromBat: '6CT-60VL 0 LB',
-        productCodeFromRimbat: 'B090 141 09 0 L',
-    }
+        productCode: '060 322 K11 #O',
+        productCodeFromBat: '560108024',
+        productCodeFromRimbat: '6СТ-60N',
+    },
+
 ]
